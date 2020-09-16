2a55cf41632b0e135e96b6ed955a5661d297d2fb
Rune Nielsen <runeanielsen@live.dk>
2020-09-16 20:05:36 +0200

    adds sln file

---
+++ src/OpenFTTH.Desktop.Bridge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenFTTH.Desktop.Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
