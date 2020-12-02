386dd652ed3735794f4f93558a06f409373e35a2
Rune Nielsen <runenielsen@runbox.com>
2020-12-02 17:15:28 +0100

    removes position filepath

--- scripts/set-environment-minikube.sh
+++ scripts/set-environment-minikube.sh
@@ -2,7 +2,6 @@
 
 # Kafka
 export KAFKA__SERVER=$(minikube ip):$(kubectl describe service openftth-kafka-cluster-kafka-external-bootstrap -n openftth | grep NodePort | grep -o '[0-9]\+')
-export KAFKA__POSITIONFILEPATH="/tmp/"
 export KAFKA__NOTIFICATIONGEOGRAPHICALAREAUPDATEDCONSUMER="notification-geographical-area-updated-desktop-bridge-consumer"
 export KAFKA__NOTIFICATIONGEOGRAPHICALAREAUPDATED="notification.geographical-area-updated"
 
