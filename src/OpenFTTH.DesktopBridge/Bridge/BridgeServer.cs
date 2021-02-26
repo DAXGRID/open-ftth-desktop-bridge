d670e3da8bee9b51dade9698ee2ebc3156986507
Rune Nielsen <runenielsen@runbox.com>
2021-02-26 23:16:04 +0100

    now pings the clients every 30 sec

--- src/OpenFTTH.DesktopBridge/Bridge/BridgeServer.cs
+++ src/OpenFTTH.DesktopBridge/Bridge/BridgeServer.cs
@@ -4,18 +4,22 @@ using NetCoreServer;
 using Microsoft.Extensions.Logging;
 using MediatR;
 using OpenFTTH.DesktopBridge.Event;
-using System;
 using System.Threading;
 
 namespace OpenFTTH.DesktopBridge.Bridge
 {
-    public class BridgeServer : WsServer, IBridgeServer, IDisposable
+    public class BridgeServer : WsServer, IBridgeServer
     {
         private readonly ILogger<BridgeServer> _logger;
         private readonly IMediator _mediator;
         private readonly IEventMapper _eventMapper;
+        private Timer _timer;
 
-        public BridgeServer(IPAddress address, int port, ILogger<BridgeServer> logger, IMediator mediator, IEventMapper eventMapper) : base(address, port)
+        public BridgeServer(IPAddress address,
+                            int port,
+                            ILogger<BridgeServer> logger,
+                            IMediator mediator,
+                            IEventMapper eventMapper) : base(address, port)
         {
             _logger = logger;
             _mediator = mediator;
@@ -25,8 +29,13 @@ namespace OpenFTTH.DesktopBridge.Bridge
 
         protected override TcpSession CreateSession()
         {
-            var t = new Timer(o => Ping(), null, 0, 30000);
             return new BridgeSession(this, _logger, _mediator, _eventMapper);
+        }
+
+        protected override void OnStarted()
+        {
+            base.OnStarted();
+            _timer = new Timer(o => Ping(), null, 0, 30000);
         }
 
         protected override void OnError(SocketError error)
@@ -34,8 +43,14 @@ namespace OpenFTTH.DesktopBridge.Bridge
             _logger.LogError($"Chat WebSocket server caught an error with code {error}");
         }
 
+        protected override void OnStopped()
+        {
+            _timer.Dispose();
+        }
+
         private void Ping()
         {
+            _logger.LogInformation("Pinging clients");
             SendPing("ping");
         }
     }
