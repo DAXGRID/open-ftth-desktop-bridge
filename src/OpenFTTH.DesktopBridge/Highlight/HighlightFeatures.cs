154b20e888408b85a43fd19428b2bc702690dfca
Rune Nielsen <runenielsen@runbox.com>
2022-05-03 15:32:43 +0200

    adds etrs89 to highlight features (#32)

--- src/OpenFTTH.DesktopBridge/Highlight/HighlightFeatures.cs
+++ src/OpenFTTH.DesktopBridge/Highlight/HighlightFeatures.cs
@@ -1,12 +1,15 @@
-using System.Collections.Generic;
 using MediatR;
+using System.Collections.Generic;
 
 namespace OpenFTTH.DesktopBridge.Highlight;
+
+public record Etrs89(float MinX, float MinY, float MaxX, float MaxY);
 
 public class HighlightFeatures : IRequest<Unit>
 {
     public string EventType { get; set; }
     public IEnumerable<string> IdentifiedFeatureMrids { get; set; }
+    public Etrs89 Etrs89 { get; set; }
     public string FeatureType { get; set; }
     public string Username { get; set; }
 }
